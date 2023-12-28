511bb5a27cfa9175f4090caf20ad7f9728e231a6
Leon Beeser <leon.beeser@yahoo.de>
2023-12-28 20:12:59 +0100

    improved readability

--- src/client.lua
+++ src/client.lua
@@ -2,50 +2,58 @@ local function setPedEquipment(ped, component, equipment, texture)
     SetPedComponentVariation(ped, component, equipment, texture, 0)
 end
 
-local function updateEquipment(ped_data)
+local function setEquipment(cache, ped, ped_weapon, component, weapons)
+    local weapon = weapons[ped_weapon]
+    if weapon == nil then
+        return cache
+    end
+
+    local ped_equipment_id = GetPedDrawableVariation(ped, component)
+
+    for _, equipment in ipairs(weapons[ped_weapon]) do
+        if equipment.id_holstered == ped_equipment_id or equipment.id_drawn == ped_equipment_id then
+            -- TODO: figure out how to refactor skipp/return to improve readability e.g. if not -> continue
+
+            setPedEquipment(ped, component, equipment.id_drawn, equipment.texture_drawn)
+
+            cache.component = component
+            cache.equipment = equipment
+
+            return cache
+        end
+    end
+end
+
+local function updateEquipment(cache)
     local ped = GetPlayerPed(-1)
     local ped_weapon = GetSelectedPedWeapon(ped)
 
-    if ped_weapon == ped_data.weapon then
-        return ped_data
+    if ped_weapon == cache.weapon then
+        return cache
     end
 
-    ped_data.weapon = ped_weapon
+    cache.weapon = ped_weapon
 
-    if ped_data.component ~= nil then
-        setPedEquipment(ped, ped_data.component, ped_data.equipment.id_holstered, ped_data.equipment.texture)
-        ped_data.component = nil -- TODO: do i need to set component to nil?
+    if cache.component ~= nil then
+        setPedEquipment(ped, cache.component, cache.equipment.id_holstered, cache.equipment.texture)
+        cache.component = nil -- TODO: do i need to set component to nil?
     end
 
     if supported_weapons_hash[ped_weapon] == nil then
-        return ped_data
+        return cache
     end
 
     local ped_supported_equipment = supported_equipment[GetEntityModel(ped)]
 
     if ped_supported_equipment == nil then
-        return ped_data
+        return cache
     end
 
     for component, weapons in pairs(ped_supported_equipment) do
-        local weapon = weapons[ped_weapon]
-        if weapon ~= nil then
-            local ped_equipment_id = GetPedDrawableVariation(ped, component)
-
-            for _, equipment in ipairs(weapons[ped_weapon]) do
-                if equipment.id_holstered == ped_equipment_id or equipment.id_drawn == ped_equipment_id then
-                    -- TODO: figure out how to refactor skipp/return to improve readability e.g. if not -> continue
-
-                    setPedEquipment(ped, component, equipment.id_drawn, equipment.texture_drawn)
-
-                    ped_data.component = component
-                    ped_data.equipment = equipment
-
-                    return ped_data
-                end
-            end
-        end
+        cache = setEquipment(cache, ped, ped_weapon, component, weapons)
     end
+
+    return cache
 end
 
 Citizen.CreateThread(function()
